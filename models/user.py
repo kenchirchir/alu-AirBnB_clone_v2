8c090cd4068f1a7937a6c5b89b9575b6cd234a60
Kenneth Chirchir <169036995+kenchirchir@users.noreply.github.com>
2025-01-25 17:57:14 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """User class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 
 
@@ -19,5 +19,11 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user', cascade='all, delete-orphan')
-    reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
+    places = relationship(
+        'Place',
+        backref='user',
+        cascade='all, delete-orphan')
+    reviews = relationship(
+        'Review',
+        backref='user',
+        cascade='all, delete-orphan')
